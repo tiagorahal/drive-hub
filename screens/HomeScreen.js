c6760416db7af246e6761bc51b8c19eabc615b50
tiagorahal <rahal.aires@gmail.com>
2024-03-22 16:45:23 -0300

    Add logo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,24 @@
-import { StyleSheet, Text, SafeAreaView } from 'react-native'
-import React from 'react'
-import tw from 'twrnc'
+import { StyleSheet, Text, SafeAreaView, Image, View } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <Text style={tw``}>I am the HomeScreen</Text>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={require("../assets/logodrivehub.png")}
+        />
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
