747f609977c8687cd053b85bd27765ee5e6cd611
tiagorahal <rahal.aires@gmail.com>
2024-03-22 15:50:34 -0300

    Finish to implement Redux

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
+import { store } from "./store";
 
 export default function App() {
   return (
-    <Provider store={}>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Lets build DriveHub!</Text>
       </View>
--- store.js
+++ store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import navReducer from "./slices/navSlice.js";
 
 export const store = configureStore({
-    reducer: {
-        nav: navReducer,
-    },
+  reducer: {
+    nav: navReducer,
+  },
 });
